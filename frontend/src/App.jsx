919a01e762521a958a5df9244ccf79d3329f3160
Abhinav-378 <abhichoukse05@gmail.com>
2024-07-19 14:34:22 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import NavBar from './Components/Header/NavBar'
+import Emergency from './Components/Emergency'
+function App() {
+ 
+
+  return (
+    <>
+      <NavBar />
+      <Emergency />
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/Components/Emergency.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Emergency() {
+  return (
+    <div>
+      <div>
+        <h1>Securing Medical Emergency by booking an Ambulace</h1>
+        <p>In urgent medical emergency, Book emergency services</p>
+        <button className="btn">Button</button>
+      </div>
+    </div>
+  )
+}
+
+export default Emergency
